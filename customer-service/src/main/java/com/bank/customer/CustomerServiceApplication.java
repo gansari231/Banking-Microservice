22712440af63fbe2752544a780273f12bf78519f
gansari231 <ansarigufraan0@gmail.com>
2026-08-20 00:27:31 +0530

    feature integrated customer service with config server

--- customer-service/src/main/java/com/bank/customer/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/bank/customer/CustomerServiceApplication.java
@@ -2,9 +2,13 @@ package com.bank.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
+
+	@Value("${test.message}")
+	private String testMessage;
 
 	public static void main(String[] args) {
 
